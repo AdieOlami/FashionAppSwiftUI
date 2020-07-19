0d06398fc1d572cc54ec3146e12cbf7b254ac8db
Adie Olalekan <ugbeadie@gmail.com>
2020-07-19 21:20:06 +0100

    Initial Commit

---
+++ FashionAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FashionAppSwiftUI
+//
+//  Created by Adie Olami on 7/19/20.
+//  Copyright © 2020 Adie Olami. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
